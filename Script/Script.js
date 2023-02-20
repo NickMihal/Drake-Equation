4610f4915fdf99b40ee1b2050f050aebe837dd31
NickMihal <86963425+NickMihal@users.noreply.github.com>
2023-02-20 15:42:56 +0200

    update

--- Script/Script.js
+++ Script/Script.js
@@ -153,12 +153,3 @@ $(document).ready(function () {
       collapse.classList.remove("show");
    }
  }
- function isNumber(evt) {
-    evt = (evt) ? evt : window.event;
-    var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-        return false;
-    }
-    return true;
- }
- 
